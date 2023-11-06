fc98557820ab1c927c1720c5e88bba5e9392ae73
ZaibLComrade <kmjahanzaib@gmail.com>
2023-11-06 14:06:10 +0600

    Added post API for users

--- index.js
+++ index.js
@@ -22,12 +22,43 @@ const app = express();
 const database = client.db('lexiplenum')
 
 // Middlewares
-app.use(cors()); // to exchange data between cross origins
+app.use(express.json());
+app.use(cors({
+	origin: ["http://localhost:5173"],
+	credentials: true,
+})); // to exchange data between cross origins
+
+// async function runDB() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+
+// 	  const books = await booksCollection.find().toArray();
+// 	  const categories = await categoryCollection.find().toArray();
+// 	  const users = await usersCollection.find().toArray();
+
+// 	  console.log(users, categories, books);
+
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// runDB().catch(console.dir);
 
 // Data collections
 const usersCollection = database.collection("users");
 const booksCollection = database.collection("books");
 const categoryCollection = database.collection("categories");
+
+// Book categories API
+app.get("/books/categories", async(req, res) => {
+	const categories = await categoryCollection.find().toArray();
+	res.send(categories);
+})
 
 // CRUD operation APIs for books
 app.get("/books", async(req, res) => {
@@ -37,6 +68,7 @@ app.get("/books", async(req, res) => {
 
 app.post("/books", async(req, res) => {
 	const newBook = req.body;
+	console.log(newBook)
 	const result = await booksCollection.insertOne(newBook);
 	res.send(result);
 })
@@ -64,27 +96,18 @@ app.delete("/books", async(req, res) => {
 	res.send(result);
 })
 
-// async function runDB() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
+// Users APIs
+app.post("/users", async(req, res) => {
+	const newUser = req.body;
+	const result = await usersCollection.insertOne(newUser);
+	res.send(result)
+})
 
-// 	  const books = await booksCollection.find().toArray();
-// 	  const categories = await categoryCollection.find().toArray();
-// 	  const users = await usersCollection.find().toArray();
-
-// 	  console.log(users, categories, books);
-
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// runDB().catch(console.dir);
-
+// test APIs
+app.get("/users/:email", (req, res) => {
+	console.log(req.params.email);
+	res.send("Hit");
+})
 
 // Root (test)
 app.get('/', (req, res) => {
